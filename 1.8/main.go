5fd1d294d630e60718dacd9afeeed173223b53d1
Rootul Patel <rootulp@gmail.com>
2020-05-29 18:36:43 -0400

    Solve exercise 1.8

---
+++ 1.8/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func main() {
+	const UrlScheme = "http://"
+	for _, url := range os.Args[1:] {
+		var urlWithScheme string
+		if strings.HasPrefix(url, UrlScheme) {
+			urlWithScheme = url
+		} else {
+			urlWithScheme = UrlScheme + url
+		}
+		resp, err := http.Get(urlWithScheme)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+		b, err := ioutil.ReadAll(resp.Body)
+		resp.Body.Close()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+			os.Exit(1)
+		}
+		fmt.Printf("%s", b)
+	}
+}
