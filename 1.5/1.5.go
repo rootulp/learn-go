58b9ada1a5e287e54f84bedee28a1bfad92ba134
Rootul Patel <rootulp@gmail.com>
2020-05-29 14:51:48 -0400

    Dir per exercise

--- 1.5/1.5.go
+++ 1.5/1.5.go
@@ -13,7 +13,7 @@ import (
 	"time"
 )
 
-var palette = []color.Color{color.White, color.Black}
+var palette = []color.Color{color.Black, color.RGBA{0x00, 0xff, 0x00, 0xff}}
 
 const (
 	whiteIndex = 0 // first color in palette
